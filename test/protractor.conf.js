6307489a57a6c8091284248ae6505ac55ea8ec83
Oleksandr Halichenko <Oleksandr.Halichenko@wolterskluwer.com>
2019-09-03 13:05:13 +0300

    fix for cssExactText

--- test/protractor.conf.js
+++ test/protractor.conf.js
@@ -1,4 +1,5 @@
 exports.config = {
+    chromeDriver: require("../node_modules/webdriver-manager-replacement/downloads/chromedriver.config.json").last,
     directConnect: true,
     specs: ['protractor.test.js'],
     capabilities: {
